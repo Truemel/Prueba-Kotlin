3fda929bc7d4b7aa9bf2b7a6495207d82fd041b9
José Ignacio <tgmelipilla@gmail.com>
2020-09-02 10:39:54 -0400

    Fifth Commit

--- app/src/main/java/com/example/pruebakotlin/modelo/HeroDAO.kt
+++ app/src/main/java/com/example/pruebakotlin/modelo/HeroDAO.kt
@@ -1,17 +1,14 @@
 package com.example.pruebakotlin.modelo
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
+import androidx.room.*
 
 @Dao
 interface HeroDAO {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertHeroList(list:MutableList<SuperheroCharacter>)
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertHero(hero:SuperheroCharacter)
 
     @Update
